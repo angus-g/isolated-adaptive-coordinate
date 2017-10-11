81649fa0e4f1862751d16ea40310b8e748b63b90
Angus Gibson <angus@agibson.me>
2017-10-11 12:33:57 +1100

    Change wrapper to remap an entire array
    Because we pass arrays as pointers to the Fortran routines, the data
    of interest must be contiguous in memory.  This isn't the case for
    k-indexed data, so we must introduce a copy to a contiguous array at
    some point.  Because the use-case of the remapping code is almost
    exclusively to remap the entire state of an array, we perform this at
    the wrapper level, simplifying the Python interface to the remapping.

--- remapping/remapping_wrap.pyx
+++ remapping/remapping_wrap.pyx
@@ -5,16 +5,34 @@ cdef extern:
                     int n1, double *h1, double *u1)
 
 def remap(int scheme,
-          double[:] h0 not None,
-          double[:] u0 not None,
-          double[:] h1 not None):
-    cdef int n0, n1
-    n0 = len(h0)
-    n1 = len(h1)
+          double[::1,:,:] h0 not None,
+          double[::1,:,:] u0 not None,
+          double[::1,:,:] h1 not None):
+    cdef unsigned int nx, ny, n0, n1
+    cdef unsigned int i, j
 
-    u1 = np.empty(n1, dtype=np.double)
-    cdef double[:] res = u1
+    nx = h0.shape[0]
+    ny = h0.shape[1]
+    n0 = h0.shape[2]
+    n1 = h1.shape[2]
 
-    remap_wrap(scheme, n0, &h0[0], &u0[0], n1, &h1[0], &res[0])
+    u1 = np.empty((nx, ny, n1), dtype=np.double, order='F')
+    cdef double[::1,:,:] res = u1
+
+    ta1 = np.empty(n0, dtype=np.double)
+    ta2 = np.empty(n0, dtype=np.double)
+    ta3 = np.empty(n1, dtype=np.double)
+    ta4 = np.empty(n1, dtype=np.double)
+    cdef double[:] t1 = ta1, t2 = ta2, t3 = ta3, t4 = ta4
+
+    for j in range(ny):
+        for i in range(nx):
+            t1[:] = h0[i,j,:]
+            t2[:] = u0[i,j,:]
+            t3[:] = h1[i,j,:]
+
+            remap_wrap(scheme, n0, &t1[0], &t2[0], n1, &t3[0], &t4[0])
+
+            u1[i,j,:] = t4
 
     return u1
